c52c0303e6fee4f29343dd4d1ff2ddf97b62e8c1
Rajan Maghera <rmaghera@ualberta.ca>
2024-08-18 20:44:11 -0400

    feat: add new function

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,21 @@ fn main() {
     })
 }
 
+fn get_number() -> i32 {
+    42
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::get_number;
 
     #[test]
     fn basic_equality() {
         assert_eq!(1, 2);
+    }
+
+    #[test]
+    fn basic_number_back() {
+        assert_eq!(get_number(), 42);
     }
 }
